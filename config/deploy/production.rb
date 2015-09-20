6ebf55c5bb29f34d37d061f10bf59b6e401c35f9
Kirk Jarvis <zuuzlo@yahoo.com>
2015-09-19 21:35:06 -0500

    changed domain name to test.zuuzlo.com

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :branch, "master"
 # This is used in the Nginx VirtualHost to specify which domains
 # the app should appear on. If you don't yet have DNS setup, you'll
 # need to create entries in your local Hosts file for testing.
-set :server_name, "www.zuuzlo.com zuuzlo.com"
+set :server_name, "test.zuuzlo.com"
 # used in case we're deploying multiple versions of the same
 # app side by side. Also provides quick sanity checks when looking
 # at filepaths
