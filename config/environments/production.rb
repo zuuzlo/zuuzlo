3a6af694b7bf9dbaa7b2134c36a5a114fc79a1a1
Kirk Jarvis <zuuzlo@yahoo.com>
2015-09-21 21:33:19 -0500

    added mailgunner 9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Zuuzlo::Application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_API_KEY']
+    api_key: ENV['MAILGUN_API_KEY'],
     domain: 'mg.zuuzlo.com' }
 
 end
